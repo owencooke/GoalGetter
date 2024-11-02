4595bdfda33399f79db9b26c2f79fffe1ac8d2fb
Zack Dorward <zdorward1@gmail.com>
2024-11-02 12:28:53 -0600

    fix bug

--- app/api/kids/route.ts
+++ app/api/kids/route.ts
@@ -1,65 +1,65 @@
 // app/api/kids/route.ts
 import { db } from "@/lib/firebase/config";
-import { Kid } from "../../../types/kids";
+import { Child } from "../../../types/child";
 import { NextResponse } from "next/server";
 import {
-    collection,
-    getDocs,
-    doc,
-    deleteDoc,
-    addDoc,
-    updateDoc,
+  collection,
+  getDocs,
+  doc,
+  deleteDoc,
+  addDoc,
+  updateDoc,
 } from "firebase/firestore";
 
 export async function GET() {
-    try {
-        const snapshot = await getDocs(collection(db, "kids"));
-        const kids: Kid[] = snapshot.docs.map(
-            (doc) =>
-                ({
-                    ...doc.data(),
-                } as Kid)
-        );
-        return NextResponse.json(kids);
-    } catch (error: any) {
-        return NextResponse.json(
-            { error: "Failed to fetch kids", message: error.message },
-            { status: 500 }
-        );
-    }
+  try {
+    const snapshot = await getDocs(collection(db, "kids"));
+    const children: Child[] = snapshot.docs.map(
+      (doc) =>
+        ({
+          ...doc.data(),
+        } as Child)
+    );
+    return NextResponse.json(children);
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: "Failed to fetch children", message: error.message },
+      { status: 500 }
+    );
+  }
 }
 
 export async function POST(request: Request) {
-    try {
-        const newKid: Kid = await request.json();
-        const docRef = await addDoc(collection(db, "kids"), {
-            ...newKid,
-        });
-        return NextResponse.json({ id: docRef.id, ...newKid });
-    } catch (error: any) {
-        return NextResponse.json(
-            { error: "Failed to add kid", message: error.message },
-            { status: 500 }
-        );
-    }
+  try {
+    const newKid: Child = await request.json();
+    const docRef = await addDoc(collection(db, "kids"), {
+      ...newKid,
+    });
+    return NextResponse.json({ id: docRef.id, ...newKid });
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: "Failed to add kid", message: error.message },
+      { status: 500 }
+    );
+  }
 }
 
 export async function PUT(request: Request) {
-    try {
-        const { id, ...updatedKidData }: Kid & { id: string } =
-            await request.json();
+  try {
+    const { id, ...updatedKidData }: Child & { id: string } =
+      await request.json();
 
-        const docRef = doc(db, "kids", id);
+    const docRef = doc(db, "kids", id);
 
-        await updateDoc(docRef, {
-            ...updatedKidData,
-        });
+    await updateDoc(docRef, {
+      ...updatedKidData,
+    });
 
-        return NextResponse.json({ id, ...updatedKidData });
-    } catch (error: any) {
-        return NextResponse.json(
-            { error: "Failed to update kid", message: error.message },
-            { status: 500 }
-        );
-    }
+    return NextResponse.json({ id, ...updatedKidData });
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: "Failed to update child", message: error.message },
+      { status: 500 }
+    );
+  }
 }
