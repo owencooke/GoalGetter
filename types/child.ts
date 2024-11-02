05fcf487fe19922dc58e7fe29949a50b6e899909
wilsonuofa <wvtruong@ualberta.ca>
2024-11-02 13:23:58 -0600

    new parent and child types final for real this time

--- types/child.ts
+++ types/child.ts
@@ -2,11 +2,11 @@ import { Goal } from "./goals";
 import { DailyStats } from "./stats";
 
 export interface Child {
-  parentId: number;
-  firstName: string;
-  lastname: string;
-  phoneNumber: string;
-  lat: number;
-  long: number;
-  dailyStats: DailyStats[];
+    childId: number;
+    firstName: string;
+    lastname: string;
+    phoneNumber: string;
+    lat: number;
+    long: number;
+    dailyStats: DailyStats[];
 }
