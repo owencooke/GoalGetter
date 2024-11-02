4c4f6d3b4327b920224cfbf5fcd6c9d0aa795e39
Zack Dorward <zdorward1@gmail.com>
2024-11-02 11:45:15 -0600

    add dummy ui

---
+++ app/achievements/page.tsx
@@ -0,0 +1,8 @@
+export default function AchievementsPage() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Welcome to the achievements page!</p>
+    </div>
+  );
+}
---
+++ app/profile/page.tsx
@@ -0,0 +1,8 @@
+export default function ProfilePage() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Welcome to the profile page!</p>
+    </div>
+  );
+}
